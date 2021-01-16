567f643d039a7d9db9ff9c47e625880d5d006bc8
julioct99 <jcasallot01@gmail.com>
2021-01-16 21:53:40 +0100

    Add input to control number of results

--- src/components/ArtistList/ArtistList.js
+++ src/components/ArtistList/ArtistList.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Artist from '../Artist/Artist';
 
 const ArtistList = ({ artists }) => {
+  const [resultLimit, setResultLimit] = useState(5);
+
   return (
     <div>
       <h2>Artists</h2>
-      {artists.map((artist) => (
+      <span>Max. results: </span>
+      <input
+        onChange={(event) => {
+          setResultLimit(event.target.value);
+        }}
+        value={resultLimit}
+        type='text'
+      />
+      {artists.splice(0, resultLimit).map((artist) => (
         <Artist artist={artist} key={artist.id} />
       ))}
     </div>
--- src/components/ReleaseList/ReleaseList.js
+++ src/components/ReleaseList/ReleaseList.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Release from '../Release/Release';
 
 const ReleaseList = ({ releases }) => {
+  const [resultLimit, setResultLimit] = useState(5);
+
   return (
     <div>
       <h2>Releases</h2>
-      {releases.map((release) => (
+      <span>Max. results: </span>
+      <input
+        onChange={(event) => {
+          setResultLimit(event.target.value);
+        }}
+        value={resultLimit}
+        type='text'
+      />
+      {releases.splice(0, resultLimit).map((release) => (
         <Release release={release} key={release.id} />
       ))}
     </div>
